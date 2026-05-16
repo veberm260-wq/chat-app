899f123c5c15f3214b3ce76b6ba5485b127c3a2c
veberm260-wq <veberm260@gmail.com>
2026-05-17 01:31:44 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'costax-v1';
+const ASSETS = ['/', '/login', '/messages.html', '/profile.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
